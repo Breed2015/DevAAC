eff90c4483acc1274a1b2f5190a965f12cf5da71
Bruno Novais <cardososp@gmail.com>
2015-02-03 13:32:27 -0200

    Shop System feature development

---
+++ DevAAC/Models/Shop.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * DevAAC
+ *
+ * Automatic Account Creator by developers.pl for TFS 1.0
+ *
+ *
+ * LICENSE: Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @package    DevAAC - Shop
+ * @author     Bruno Novais <cardososp@gmail.com>
+ * @copyright  2015 Bruno Novais
+ * @license    http://opensource.org/licenses/MIT MIT
+ * @version    development
+ * @link       https://github.com/novasdream/DevAAC
+ */
+
+namespace DevAAC\Models;
+
+use DevAAC\Helpers\DateTime;
+
+// https://github.com/illuminate/database/blob/master/Eloquent/Model.php
+// https://github.com/otland/forgottenserver/blob/master/schema.sql
+
+/**
+ * @SWG\Model(required="['id','name','description','price','image_url','level_need','start_date','end_date']")
+ */
+class Shop extends \Illuminate\Database\Eloquent\Model {
+    /**
+     * @SWG\Property(name="id", type="integer")
+     * @SWG\Property(name="name", type="string")
+     * @SWG\Property(name="description", type="string")
+     * @SWG\Property(name="price", type="integer")
+     * @SWG\Property(name="image_url", type="string")
+     * @SWG\Property(name="level_need", type="integer")
+     * @SWG\Property(name="start_date", type="DateTime::ISO8601")
+     * @SWG\Property(name="end_date", type="DateTime::ISO8601")
+     */
+    public $timestamps = false;
+
+    protected $guarded = array('id');
+
+    protected $hidden = array('end_date');
+
+}
